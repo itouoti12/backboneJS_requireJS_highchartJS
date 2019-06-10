00053c791df06a38007887a4e44476f27c4548af
itouoti <white.sleipnir.jazz@gmail.com>
2019-06-10 22:07:51 +0900

    viewの方でインスタンスを生成するように修正（012）

--- 012_backboneJS/js/views/HelloWorldView.js
+++ 012_backboneJS/js/views/HelloWorldView.js
@@ -1,13 +1,15 @@
 require(['backbone', 'model/HelloWorldModel'], function(backbone, helloWorldModel) {
     //viewとして適切な分解をする
 
+    var model = new helloWorldModel();
+    
     var HelloWorldView = backbone.View.extend({
         el: $('.hoge'),
         initialize: function() {
             this.render();
         },
         render: function() {
-            var text = helloWorldModel.get("text");
+            var text = model.get("text");
             $(this.el).html(_.template($('#hello-template').text())({ "text": text}));
         }
     });
